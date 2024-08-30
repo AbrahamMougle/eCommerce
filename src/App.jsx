e73b6dd7ceaa08a714def2512fce25b44ea5c9a5
AbrahamMoule <abrahammougle76@gmail.com>
2024-08-30 21:23:05 +0200

    J'ai coupe mon application en des modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { BrowserRouter, Route, Link, Routes, useParams } from "react-router-dom";
 import './mirage.js';
-import { StarIcon } from "@heroicons/react/20/solid";
+import ProduitDetail from "./Component/ProduitDetail.jsx";
+
 
 function Vetement() {
   const [vetements, setVetements] = useState([]);
@@ -28,24 +29,7 @@ function Vetement() {
   );
 }
 
-function About(params) {
-  return <>
-    <div className=" px-2">
-      <img className="object-cover h-80 w-full" src="https://s.alicdn.com/@sc04/kf/H0538b0cfdeef4f438aa485909a6b227aR.jpg_64x64.jpg" alt="" />
-    </div>
-    <div className="px-2">
-      <h1 className="text-left">Élégance au Quotidien avec Sacs et Chaussures</h1>
-      <p className="text-lg/8">Explorez notre sélection de sacs et chaussures, alliant style et fonctionnalité. Chaque article est conçu pour vous accompagner dans votre vie quotidienne tout en ajoutant une touche d’élégance à votre tenue.</p>
-    </div>
-    <div className="flex justify-center items-center gap-4 pt-4 ">
-      <img className="size-48 rounded-lg" src="https://s.alicdn.com/@sc04/kf/H6a07aead03884220accf61c229114ca5L.jpg_64x64.jpg" alt="" />
-      <img className="size-48 rounded-lg" src="https://s.alicdn.com/@sc04/kf/H94dc32fa408043f3a2d6b30c09edf892E.jpg_64x64.jpg" alt="" />
-      <img className="size-48 rounded-lg" src="https://s.alicdn.com/@sc04/kf/H0538b0cfdeef4f438aa485909a6b227aR.jpg_64x64.jpg" alt="" />
-    </div>
-    <Link to='/produit'><button className="px-2 mt-4 text-center  rounded-xl bg-black  w-44 h-14 text-lg text-amber-50 hover:bg-slate-50 hover:text-black hover:border-2 hover:border-black">Decouvrir</button></Link>
 
-  </>
-}
 function Home(params) {
   return <h1>this my About</h1>
 }
@@ -65,35 +49,7 @@ function Navigator(params) {
     </header>
   </>
 }
-function ProduitDetail(params) {
-  const [detail, setDetail] = useState(null)
-  const { id } = useParams()
-  useEffect(() => {
-    fetch(`/api/vetements/${id}`)
-      .then((response) => response.json())
-      .then((data) => setDetail(data.vetement));
-  }, [id]);
-  console.log(detail);
-  if (!detail) return <h1>Loading</h1>; // Affiche un message pendant le chargement
-  
-  return <div className="w-1/3 mx-auto">
-    
-    <h1>{detail.nom}</h1>
-    <img className="objet-cover" src={detail.imageUrl} alt={detail.nom} />
-    <p>{detail.description}</p>
-    <p>Prix : {detail.prix} €</p>
-    <div className="flex">
-    <StarIcon className="size-6 text-yellow-600"/>
-    <StarIcon className="size-6 text-yellow-600"/>
-    <StarIcon className="size-6 text-yellow-600"/>
-    <StarIcon className="size-6 text-yellow-600"/>
-    <StarIcon className="size-6 text-black"/>
-    </div>
-    
-  </div>
 
-
-}
 
 function RouteNav() {
   return <>
